bdfdd6ca7edd5f9cef7981591d030b7b2be3020c
Jaoquin Gumucio <jjgumucio@gmail.com>
2016-07-25 21:57:43 -0400

    Improves documentation

--- server/methods/resumes.js
+++ server/methods/resumes.js
@@ -4,7 +4,7 @@ import Resumes from '../../imports/api/collections/resumes'
 
 /**
  * Methods are used to run code on the server and optionally, send a response
- * to the client (ex: APIs, do computationsm etc).
+ * to the client (ex: APIs, do computationsm, work with the DB, etc).
  * In Meteor, methods are functions declared as values of a Simple
  * object that is in turn, passed to the Meteor.methods function
  */
